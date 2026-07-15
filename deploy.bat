99a33291d870e07fbec7301de895da97d22461d6
kanzakirei <kanzakirei@aqua.gmail.com>
2026-07-16 00:47:10 +0900

    名称の修正

--- deploy.bat
+++ deploy.bat
@@ -5,8 +5,8 @@ setlocal
 call SSG.exe
 endlocal
 
-set FROM="C:\Users\kanza\OneDrive\Documents\Git\temisama-development\output"
-set TO="C:\Users\kanza\OneDrive\Documents\Git\temisama"
+set FROM="C:\Users\kanza\OneDrive\Documents\Git\tamisama-development\output"
+set TO="C:\Users\kanza\OneDrive\Documents\Git\tamisama"
 xcopy %FROM% %TO% /e /y
 
 pushd %TO%
