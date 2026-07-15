adce76fa061e64f36201685c3ac8e156b8e23aca
kanzakirei <kanzakirei@aqua.gmail.com>
2026-07-15 19:00:28 +0900

    deployツールの追加

---
+++ deploy.bat
@@ -0,0 +1,13 @@
+@echo off
+setlocal
+set FROM="C:\Users\kanza\OneDrive\Documents\Git\temisama-development\output"
+set TO="C:\Users\kanza\OneDrive\Documents\Git\temisama"
+xcopy %FROM% %TO% /e /y
+
+pushd %TO%
+git add.
+git commit -m "デプロイ"
+git push
+popd
+endlocal
+exit 0
